1b108125dad3d51b2a995eba9b464726ec70261e
Erik Vroon <erik.vroon22@gmail.com>
2021-08-17 23:02:51 +0200

    Various bugfixes and improvements

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import json
 import time
 import datetime
 import pytz
+import re
 from dotenv import load_dotenv
+from typing import List, Dict
 
 load_dotenv()
 
@@ -19,8 +21,6 @@ REDMINE_HEADERS = {
 
 GITEA_DOMAIN = os.getenv('GITEA_DOMAIN')
 GITEA_API_TOKEN = os.getenv('GITEA_API_TOKEN')
-GITEA_REPO = os.getenv('GITEA_REPO')
-GITEA_ENDPOINT = f'api/v1/repos/{GITEA_REPO}/issues'
 GITEA_HEADERS = {
     'accept': 'application/json',
     'Authorization': f'token {GITEA_API_TOKEN}',
@@ -28,16 +28,34 @@ GITEA_HEADERS = {
 }
 
 DEFAULT_USERNAME = os.getenv('DEFAULT_USERNAME')
+ORGANIZATION = os.getenv('ORGANIZATION')
+
+labels: Dict = {}
+comments_to_update = []
+
+
+def get_gitea_repo(redmine_project_name: str) -> str:
+    project_name = redmine_project_name.strip().lower()
+
+    if project_name == 'nuno':
+        project_name = 'nuno-api'
+    elif project_name == 'cotton':
+        project_name = 'tv'
+
+    return f'{ORGANIZATION}/{project_name}'
+
+
+def get_gitea_endpoint(gitea_repo: str) -> str:
+    return f'{GITEA_DOMAIN}/api/v1/repos/{gitea_repo}/issues'
 
 
 def process_issues() -> None:
     issues = get_issues()
-    labels = get_labels()
     projects = get_projects()
     users = get_users()
 
-    for issue in issues:
-        create_issue(issue, labels, projects, users)
+    for issue in issues[::-1]:
+        create_issue(issue, projects, users)
 
 
 def get_users() -> dict:
@@ -46,7 +64,7 @@ def get_users() -> dict:
     Returns:
         dict: Dict of (username, name) pairs for each user id
     """
-    response = requests.get(f'https://{REDMINE_DOMAIN}/users.json', headers=REDMINE_HEADERS)
+    response = requests.get(f'{REDMINE_DOMAIN}/users.json', headers=REDMINE_HEADERS)
     result = {}
 
     for user in json.loads(response.content)['users']:
@@ -64,7 +82,7 @@ def get_projects() -> dict:
     Returns:
         dict: Dict of project names for each id
     """
-    response = requests.get(f'https://{REDMINE_DOMAIN}/projects.json', headers=REDMINE_HEADERS)
+    response = requests.get(f'{REDMINE_DOMAIN}/projects.json', headers=REDMINE_HEADERS)
     result = {}
 
     for project in json.loads(response.content)['projects']:
@@ -73,37 +91,63 @@ def get_projects() -> dict:
     return result
 
 
-def get_issues(limit: int = 10000, offset: int = 0) -> dict:
+def get_issues() -> List:
     """Get issues from the Redmine instance.
-
-    Args:
-        limit (int, optional): Limit the number of issues returned. Defaults to 10.
-        offset (int, optional): Offset for pagination of issues. Defaults to 0.
 
     Returns:
         dict: Issues with data in key-value pairs.
     """
-    response = requests.get(f'https://{REDMINE_DOMAIN}/projects/{REDMINE_PROJECT_NAME}/' +
-        f'issues.json?status_id=*&limit={limit}&offset={offset}', headers=REDMINE_HEADERS)
+    i = 0
+    limit = 100
+    result = []
+    filename = 'issues.json'
 
-    return json.loads(response.content)['issues']
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            return json.load(f)
+
+    print('Loading issues...')
+    while True:
+        response = requests.get(f'{REDMINE_DOMAIN}/' +
+            f'issues.json?status_id=*&limit={limit}&offset={i}', headers=REDMINE_HEADERS)
+        json_response = response.json()['issues']
+
+        for issue in json_response:
+            result.append(issue)
+
+        if len(json_response) < limit:
+            break
+
+        i += len(json_response)
+
+    # Reverse order, oldest first.
+    result = result[::-1]
+
+    with open(filename, 'w') as f:
+        json.dump(result, f, sort_keys=True, indent=4)
+
+    return result
 
 
-def get_labels() -> dict:
+def get_labels(gitea_repo: str) -> dict:
     """Get the labels used for the repo in Gitea.
 
     Returns:
         dict: Pairs of (name, id) of all labels.
     """
+    if gitea_repo in labels:
+        return labels[gitea_repo]
+
     result = {}
     response = requests.get(
-        f'https://{GITEA_DOMAIN}/api/v1/repos/{GITEA_REPO}/labels',
+        f'{GITEA_DOMAIN}/api/v1/repos/{gitea_repo}/labels',
         headers=GITEA_HEADERS
     )
 
     for label in json.loads(response.content):
         result[label['name']] = label['id']
 
+    labels[gitea_repo] = result
     return result
 
 
@@ -116,13 +160,15 @@ def get_comments(issue_id: int) -> dict:
     Returns:
         [dict]: The comments of the issue
     """
-    response = requests.get(f'https://{REDMINE_DOMAIN}/' +
-        f'issues/{issue_id}.json?include=journals', headers=REDMINE_HEADERS)
+    response = requests.get(
+        f'{REDMINE_DOMAIN}/issues/{issue_id}.json?include=journals',
+        headers=REDMINE_HEADERS
+    )
 
     return json.loads(response.content)['issue']['journals']
 
 
-def add_labels(issue_id: int, labels: list, user: str) -> List:
+def add_labels(gitea_repo: str, issue_id: int, labels: list, user: str) -> List:
     """Add labels to an issue
 
     Args:
@@ -138,7 +184,7 @@ def add_labels(issue_id: int, labels: list, user: str) -> List:
     }
 
     response = requests.post(
-        f'https://{GITEA_DOMAIN}/api/v1/repos/{GITEA_REPO}/issues/{issue_id}/labels?sudo={user}',
+        f'{GITEA_DOMAIN}/api/v1/repos/{gitea_repo}/issues/{issue_id}/labels?sudo={user}',
         headers=GITEA_HEADERS,
         data=json.dumps(data)
     )
@@ -146,7 +192,20 @@ def add_labels(issue_id: int, labels: list, user: str) -> List:
     return [x['id'] for x in json.loads(response.content)]
 
 
-def add_comment(issue_id: int, body: str, user: str) -> None:
+def check_for_references(content: str, gitea_repo: str, comment_id: str) -> None:
+    matches = re.findall('(#[0-9]+)', content)
+    if matches is not None and len(matches) > 0:
+        comments_to_update.append({
+            'gitea_repo': gitea_repo,
+            'comment_id': comment_id,
+            'content': content,
+            'matches': matches,
+        })
+
+        print(f'\tFound {len(matches)} issue reference(s) in repo {gitea_repo} with comment id {comment_id}.')
+
+
+def add_comment(gitea_repo: str, issue_id: int, body: str, user: str) -> None:
     """Add comment to an issue in Gitea
 
     Args:
@@ -157,47 +216,67 @@ def add_comment(issue_id: int, body: str, user: str) -> None:
     data = {
         'body': body
     }
+    url = f'{GITEA_DOMAIN}/api/v1/repos/{gitea_repo}/issues/{issue_id}/comments?sudo={user}'
 
     response = requests.post(
-        f'https://{GITEA_DOMAIN}/api/v1/repos/{GITEA_REPO}/issues/{issue_id}/comments?sudo={user}',
+        url,
         headers=GITEA_HEADERS,
         data=json.dumps(data)
     )
 
+    # Try again without sudo.
+    if response.status_code == 404:
+        data['body'] += f'\n*Added by user: {user}*'
+        url = f'{GITEA_DOMAIN}/api/v1/repos/{gitea_repo}/issues/{issue_id}/comments'
+
+        response = requests.post(
+            url,
+            headers=GITEA_HEADERS,
+            data=json.dumps(data)
+        )
+
+
     if not response.ok:
         print(response.content)
         raise SystemExit()
+
+    check_for_references(body, gitea_repo, response.json()['id'])
 
 
 def get_username(users: dict, id: int) -> str:
     if id in users:
         return users[id]['username']
 
-    return DEFAULT_USERNAME
+    return DEFAULT_USERNAME if DEFAULT_USERNAME is not None else ''
 
 
-def create_issue(issue: dict, labels: dict, projects: dict, users: dict) -> None:
+def create_issue(issue: dict, projects: dict, users: dict) -> None:
     """Creates an issue in Gitea given the json data of Redmine's API.
 
     Args:
         issue (dict): The data of the issue from Redmine.
-        labels (dict): The labels of the repo in Gitea.
     """
     id = issue['id']
     subject = issue['subject']
+    project = projects[int(issue['project']['id'])]
     description = issue['description'].replace('\r\n', '\n')
     status = issue['status']['name']
-    custom_fields = [f'| {x["name"]} | {x["value"]} |' for x in issue['custom_fields'] if x['value'] != '']
+    custom_fields = [f'| {x["name"]} | {x["value"]} |' for x in issue['custom_fields'] if x['value'] is not None and x['value'] != '']
     issue_type = issue['tracker']['name']
     priority = issue['priority']['name']
     is_private = issue['is_private']
     done_ratio = issue['done_ratio']
+    created_on_obj = datetime.datetime.fromisoformat(issue['created_on'].replace('Z', '+00:00'))
+    created_on = created_on_obj.astimezone(pytz.timezone('Europe/Amsterdam')).strftime('%d %B %Y at %H:%M')
     category = '-'
+
+    gitea_repo = get_gitea_repo(project)
+    labels = get_labels(gitea_repo)
 
     print(id, subject)
 
     if 'category' in issue:
-        category = issue['category']
+        category = issue['category']['name']
 
     assigned_to = '-'
     if 'assigned_to' in issue:
@@ -217,7 +296,8 @@ def create_issue(issue: dict, labels: dict, projects: dict, users: dict) -> None
 *Imported from Redmine*
 | Property      | Value             |
 | ---           | ---               |
-| ID            | #{id}             |
+| Originele ID  | {id}              |
+| Created On    | {created_on}      |
 | Priority      | {priority}        |
 | Status        | {status}          |
 | Issue type    | {issue_type}      |
@@ -244,12 +324,16 @@ def create_issue(issue: dict, labels: dict, projects: dict, users: dict) -> None
         'Support': 'support',
     }
     data['labels'] = [labels[issue_type_map[issue_type]]]
+    data['labels'].append(labels['migrated'])
 
     if status == 'Rejected':
         data['labels'].append(labels['wontfix'])
 
+    data['labels'].sort()
+    endpoint = get_gitea_endpoint(gitea_repo)
+
     response = requests.post(
-        f'https://{GITEA_DOMAIN}/{GITEA_ENDPOINT}?sudo={author_username}',
+        f'{endpoint}?sudo={author_username}',
         headers=GITEA_HEADERS,
         data=json.dumps(data)
     )
@@ -257,15 +341,19 @@ def create_issue(issue: dict, labels: dict, projects: dict, users: dict) -> None
     if response.status_code == 422:
         print(f'\tCould not assign assignee: {assigned_to_username}, retrying without assignee...')
         del data['assignee']
-    response = requests.post(
-        f'https://{GITEA_DOMAIN}/{GITEA_ENDPOINT}?sudo={author_username}',
+
+        response = requests.post(
+            f'{endpoint}?sudo={author_username}',
             headers=GITEA_HEADERS,
-        data=json.dumps(data)
-    )
+            data=json.dumps(data)
+        )
 
     if not response.ok:
         print(f'Error while creating issue ({response.status_code}):', response.content)
+        print(data)
         raise SystemExit()
+
+    check_for_references(description, gitea_repo, 'description')
 
     response_json = json.loads(response.content)
     gitea_issue_id = response_json['number']
@@ -273,10 +361,12 @@ def create_issue(issue: dict, labels: dict, projects: dict, users: dict) -> None
 
     if 'labels' in response_json:
         gitea_labels = [x['id'] for x in response_json['labels']]
+        gitea_labels.sort()
+
         while gitea_labels != data['labels']:
             print(f'\tCreated labels ({gitea_labels}) do not match expected labels ({data["labels"]}) for gitea issue {gitea_issue_id}, retrying...')
             time.sleep(0.5)
-            gitea_labels = add_labels(gitea_issue_id, data['labels'], author_username)
+            gitea_labels = add_labels(gitea_repo, gitea_issue_id, data['labels'], author_username)
 
     map_status = {
         1: 'New',
@@ -337,9 +427,9 @@ def create_issue(issue: dict, labels: dict, projects: dict, users: dict) -> None
                 new_value = projects[int(new_value)]
 
             if property_name == 'assigned_to_id':
-                if old_value != None:
+                if old_value != None and int(old_value) in users:
                     old_value = users[int(old_value)]['name']
-                if new_value != None:
+                if new_value != None and int(new_value) in users:
                     new_value = users[int(new_value)]['name']
 
             if not property_name.isnumeric():
@@ -351,7 +441,8 @@ def create_issue(issue: dict, labels: dict, projects: dict, users: dict) -> None
                     detail_text.append(f'*`{property_name}` changed from {prefix_old}{old_value}{unit} to {prefix_new}{new_value}{unit}*')
 
         body = notes + ('\n***\n' if len(notes) > 0 and len(detail_text) > 0 else '') + '\n'.join(detail_text) + f'\n\n*Date: {created_on}*'
-        add_comment(gitea_issue_id, body, user)
+        add_comment(gitea_repo, gitea_issue_id, body, user)
 
 
-process_issues()
+if __name__ == '__main__':
+    process_issues()
